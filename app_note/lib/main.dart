0916a9e0d0baef1b6647ab453246c72cf9543f9d
Abdualsslam <abdualsslam222@gmail.com>
2024-11-10 23:55:40 +0300

    adding MyBlocObserver

--- app_note/lib/main.dart
+++ app_note/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:app_note/constans.dart';
 import 'package:app_note/cubits/cubit/add_note_cubit.dart';
 import 'package:app_note/models/note_model.dart';
+import 'package:app_note/simple_bloc_observer.dart';
 import 'package:app_note/view/notes_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
-  await Hive.initFlutter(kNotesBox);
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
+  Bloc.observer = MyBlocObserver();
   runApp(const NotesApp());
 }
 
