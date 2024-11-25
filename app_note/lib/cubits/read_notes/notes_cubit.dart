902659935ca671da1bd3363939f7bdf7e6958a91
Abdualsslam <abdualsslam222@gmail.com>
2024-11-25 08:11:31 +0300

    create fetch notes method

--- app_note/lib/cubits/read_notes/notes_cubit.dart
+++ app_note/lib/cubits/read_notes/notes_cubit.dart
@@ -1,9 +1,20 @@
+import 'package:app_note/constans.dart';
 import 'package:app_note/models/note_model.dart';
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchAllNotes() async {
+    // emit(NotesLoading());  no need to await its localy
+    try {
+      var noteBox = Hive.box<NoteModel>(kNotesBox);
+      emit(NotesSuccess(noteBox.values.toList()));
+    } on Exception catch (e) {
+      emit(NotesFailure(errorMessage: e.toString()));
+    }
+  }
 }
--- app_note/lib/view/notes_view.dart
+++ app_note/lib/view/notes_view.dart
@@ -1,36 +1,41 @@
+import 'package:app_note/cubits/read_notes/notes_cubit.dart';
 import 'package:app_note/view/widgets/floaing_button.dart';
 import 'package:app_note/view/widgets/notes_view_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            context: context,
-            builder: (context) {
-              return SingleChildScrollView(
-                  child: Padding(
-                padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context)
-                      .viewInsets
-                      .bottom, // يحدد حافة للجزء السفلي بحجم لوحة المفاتيح
-                ),
-                child: const FloatingButton(),
-              ));
-            },
-          );
-        },
-        child: const Icon(Icons.add),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              context: context,
+              builder: (context) {
+                return SingleChildScrollView(
+                    child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context)
+                        .viewInsets
+                        .bottom, // يحدد حافة للجزء السفلي بحجم لوحة المفاتيح
+                  ),
+                  child: const FloatingButton(),
+                ));
+              },
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const NoteViewBody(),
       ),
-      body: const NoteViewBody(),
     );
   }
 }
