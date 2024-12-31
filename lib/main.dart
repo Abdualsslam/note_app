e4ff65195e8a3ab1839255f3f5dfa2a780598b99
Abdualsslam <abdualsslam222@gmail.com>
2025-01-01 01:13:41 +0300

    Migrated project to new Flutter setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Scaffold(
+        body: SizedBox(),
+      ),
+    );
+  }
+}
